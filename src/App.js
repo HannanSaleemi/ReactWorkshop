a8739cf024c60a5531102e2e9685252ba396b5b1
Hannan Saleemi <hannan@octopuslabs.com>
2020-03-07 09:48:20 +0000

    subsrcibed to timer events

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { subscribeToTimer } from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    subscribeToTimer((err, timestamp) => this.setState({
+      timestamp
+    }));
+  }
+
+  state = {
+    timestamp: 'no timestamp yet'
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>This is the time value: {this.state.timestamp}</p>
+      </div>
+    )
+  }
 }
 
 export default App;
